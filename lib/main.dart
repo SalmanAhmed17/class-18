5c3242454aed6d5877658f405efdeec7ffb559b4
Salman Ahmed <93308774+SalmanAhmed17@users.noreply.github.com>
2023-02-06 15:38:03 +0600

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:class18/fract.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Frac(),
+    );
+  }
+}
